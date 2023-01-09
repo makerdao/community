332a93df5e4f5571952847709da75190862b51fc
LongForWisdom <LongForWisdom@pm.me>
2023-01-09 15:42:08 +0000

    Nuked the repo.
    * Removed out of date content.
    * Removed publishing-related content.
    * Now just governance stuff + introductory content (also out of date, but best we have)

--- src/modules/localization/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useTranslation } from './hooks/translation';
-export { default as TranslationProvider } from './TranslationProvider';
-export { TranslationContext } from './TranslationProvider';
-export const DEFAULT_LOCALE = 'en'; //<- Set your default locale for the application. Make sure it matches the locale folder.
